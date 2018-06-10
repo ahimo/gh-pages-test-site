36347a39c2eec219f9f9e6a1dd375ced3014cdb1
Your Name <you@example.com>
2018-06-10 22:53:47 +0900

    page2を追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 
 import page1 from './page1.js'
+import page2 from './page2.js'
 
 const HelloWorld = ()=>(
     <h1>Hello, World!</h1>
@@ -13,6 +14,7 @@ const App = ()=>(
         <div>
             <Route exact path='/'      component={HelloWorld} />
             <Route       path='/page1' component={page1} />
+            <Route       path='/page2' component={page2} />
         </div>
     </BrowserRouter>
 )
---
+++ src/page2.js
@@ -0,0 +1,11 @@
+// src/page2.js
+import React from 'react'
+
+const page2 = ()=>(
+    <div>
+        <h2>Page 2</h2>
+        <p>ページ2です.</p>
+    </div>
+)
+
+export default page2
