741b2247c4e3e39392de785e354db595e39c54c2
Your Name <you@example.com>
2018-06-10 22:24:15 +0900

    ページ追加対応

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 // src/App.js
 import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import page1 from './page1.js'
 
 const HelloWorld = ()=>(
     <h1>Hello, World!</h1>
 )
 
-export default HelloWorld
+const App = ()=>(
+    <BrowserRouter>
+        <div>
+            <Route exact path='/'      component={HelloWorld} />
+            <Route       path='/page1' component={page1} />
+        </div>
+    </BrowserRouter>
+)
+
+export default App
---
+++ src/page1.js
@@ -0,0 +1,11 @@
+// src/page1.js
+import React from 'react'
+
+const page1 = ()=>(
+    <div>
+        <h2>Page 1</h2>
+        <p>ページ1です.</p>
+    </div>
+)
+
+export default page1
